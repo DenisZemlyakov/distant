c51e91f78a05f348dc0a50857bea8a0bce800525
Denis Zemlyakov <denisino@bk.ru>
2021-11-18 10:34:38 +0200

    distant

--- PythonApplication1den4ik/PythonApplication1den4ik.py
+++ PythonApplication1den4ik/PythonApplication1den4ik.py
@@ -2,6 +2,21 @@ from myymodule1 import*
 user=["Denis"]
 password=["12345"]
 
-password=passautomat()
-print(password)
+while True:
+    print("reg-1,avt-2,valja-3")
+    V=int(input())
+    if V==1:
+        print("registreerimine")
+        #
+    elif V==2:
+        print("avt")
+        #
+    elif V==3:
+        print("valja")
+        break
+        #valmis
+    else:
+        print("on vaja valida 1,2 voi 3")#всё
+
+
 
